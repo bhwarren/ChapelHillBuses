b0bf2406ed79591549e3dd2554fb111a00db5ceb
Bo Warren <linuxcoyote46@gmail.com>
2014-11-08 23:41:14 -0500

    added most functionality, and cleaned up some things

--- client_side/showBuses.js
+++ client_side/showBuses.js
@@ -6,29 +6,35 @@ $(document).ready(function () {
                 this.arrival1=arrival1;
                 this.arrival2=arrival2;
         }   
-        var result1 = new routeDisplay("A","Longview","To family practice", "13 minutes", "37 minutes");
-        var result2 = new routeDisplay("NS","MLK","To family practice", "3 minutes", "47 minutes");
-        var result3 = new routeDisplay("RU","Spencer Dorm","Student Stores", "5 minutes", "28 minutes");
+        var result1 = new routeDisplay("A","Student Union","To family practice", "13 minutes", "37 minutes");
+        var result2 = new routeDisplay("NS","Sitterson","To family practice", "3 minutes", "47 minutes");
+        var result3 = new routeDisplay("RU","Spencer Dorm","To Student Union", "5 minutes", "28 minutes");
+        var result4 = new routeDisplay("U","Student Union","To Franklin Street", "15 minutes", "48 minutes");
 
+	var results = [result1,result2,result3, result4];
 
-	$(document).on('change', 'div#select_by_r_and_s > p > select.route_select', function(e) {
-
-		var cur_route=$("div#select_by_r_and_s > p > select.route_select :selected").text();
-		if(cur_route==result1.route){
-			$("#results").html(generate_route_html(result1));
-		}
-		if(cur_route==result2.route){
-			$("#results").html(generate_route_html(result2));
-		}
-		if(cur_route==result3.route){
-			$("#results").html(generate_route_html(result3));
-		}
-	});
+	select_bus();
+	$(document).on('change', 'div#select_by_r_and_s > p > select.stop_select', select_bus); 
+	$(document).on('change', 'div#select_by_r_and_s > p > select.route_select', select_bus); 
 
         function generate_route_html(route_Display){
                 return "<p class=\"route_display\">"+route_Display.route+ "<br>"+route_Display.stop+"<br>"+
 route_Display.direction+"<br>"+route_Display.arrival1+"<br>"+route_Display.arrival2+"</p>";
 
         }   
+	function select_bus(e) {
+		var cur_stop = $("div#select_by_r_and_s > p > select.stop_select :selected").text();
+                var cur_route=$("div#select_by_r_and_s > p > select.route_select :selected").text();
 
-})
+		$("#results").html("");
+		for(var i=0; i<results.length; i++){
+			//have to keep the ifs separate, otherwise doesn't work ????
+			if(cur_stop == results[i].stop ){
+				if( cur_route == results[i].route || cur_route == "All Buses"){
+					$("#results").append(generate_route_html(results[i]));
+				}
+			}
+		}		
+        }	
+
+});
